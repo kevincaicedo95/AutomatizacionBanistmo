7ad47f88921d78d14e80af5dc31ac4c39316c66c
kevin Caicedo <Kevin950922@gmail.com>
2024-08-04 14:57:51 -0500

    configurando proyecto

---
+++ src/test/java/stepDefinitions/PdfProhibiciones.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class PdfProhibiciones {
+    @Dado("que el usuario esta en la pagina principal de personas")
+    public void queElUsuarioEstaEnLaPaginaPrincipalDePersonas() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Cuando("el usuario navega hasta tasas y tarifas")
+    public void elUsuarioNavegaHastaTasasYTarifas() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Entonces("valido que se pueda descargar correctamente el pdf de prohibiciones")
+    public void validoQueSePuedaDescargarCorrectamenteElPdfDeProhibiciones() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ src/test/resources/features/pdfProhibiciones.feature
@@ -0,0 +1,6 @@
+#language:es
+  Característica: tasas y tarifas
+    Escenario: Validar correcta descarga del pdf de prohibiciones
+      Dado que el usuario esta en la pagina principal de personas
+      Cuando el usuario navega hasta tasas y tarifas
+      Entonces valido que se pueda descargar correctamente el pdf de prohibiciones
